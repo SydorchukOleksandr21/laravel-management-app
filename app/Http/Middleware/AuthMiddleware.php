5d416c112804f6d7b56e6a9707576e7159383b14
Alexander Sydorchuk <diguni_sos@ztu.edu.ua>
2025-03-27 16:00:17 +0200

    [FIX] Middleware fixed

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthMiddleware
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check()) {
+            return redirect()->route('auth.login');
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -27,5 +28,5 @@ Route::prefix('auth')
         Route::post('login', 'loginUser')->name('loginUser');
         Route::post('signup', 'createUser')->name('createUser');
 
-        Route::delete('logout', 'logout')->name('logout')->middleware('auth');
+        Route::delete('logout', 'logout')->name('logout')->middleware(AuthMiddleware::class);
     });
