7bc1cc4e71c9927307ad58d13ca7978c9d97d98f
Alexander Sydorchuk <diguni_sos@ztu.edu.ua>
2025-04-17 16:01:28 +0300

    [FEATURE] trsnsaction multiple saving

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\base\Controller;
+use App\Http\Controllers\base\ResourceController;
 use App\Models\Room;
+use App\Models\RoomSample;
 use App\Services\Core\ModelSearch;
 use Illuminate\Http\Request;
-
-class RoomController extends Controller
+use Illuminate\Support\Facades\DB;
+class RoomController extends ResourceController
 {
     /**
      * Display a listing of the resource.
@@ -21,6 +23,7 @@ class RoomController extends Controller
             'className' => Room::class,
         ]);
     }
+
     /**
      * Show the form for creating a new resource.
      */
@@ -59,6 +62,26 @@ class RoomController extends Controller
     public function update(Request $request, string $id)
     {
         //
+    }
+
+    private function createRoom(RoomSample $roomSample, Request $request, bool $isNew)
+    {
+        $amount = intval($request->get('rooms_count'));
+        $rooms = [];
+
+        DB::transaction(function () use (&$rooms, $amount, $request, $roomSample) {
+            for ($i = 0; $i < $amount; $i++) {
+                $request->merge(['number' => 5]); // приклад — можна адаптувати
+
+                $rooms[] = $this->save($roomSample->replicate(), $request, [
+                    'number' => 'required|integer|min:1',
+                    'floor' => 'required|integer|min:1',
+                    'note' => 'nullable|string',
+                ], false);
+            }
+        });
+
+        return $rooms;
     }
 
     /**
